97d22c1e47f82480657be31ff97bd2d60f62c737
xavierwoo <xavier.woo@gmail.com>
2017-07-03 15:06:10 -0700

    Added a greedy algorithm for TSP

---
+++ src/lkhj/GreedyTSP.java
@@ -0,0 +1,48 @@
+package lkhj;
+
+
+/**
+ * A greedy algorithm for TSP problem
+ * Created by xinyun on 03/07/17.
+ */
+public class GreedyTSP {
+    private double[][] costMatrix;
+    private int[] tour;
+    private boolean[] isInTour;
+
+    public GreedyTSP(double[][] costMatrix){
+        this.costMatrix = costMatrix;
+        tour = new int[costMatrix.length];
+        isInTour = new boolean[costMatrix.length];
+    }
+
+    public int[] solve(){
+        tour[0] = 0;
+        isInTour[0] = true;
+        int count = 1;
+        while(count < tour.length){
+            int currN = tour[count-1];
+            int nextN = findNearest(currN);
+            tour[count] = nextN;
+            isInTour[count] = true;
+            ++count;
+        }
+        return tour;
+    }
+
+    private int findNearest(int n){
+        double smallestF = Double.MAX_VALUE;
+        int nearestN = -1;
+
+        for (int i=0; i < tour.length; ++i){
+            if (i != n && !isInTour[i]){
+                double f = costMatrix[n][i];
+                if (f < smallestF){
+                    smallestF = f;
+                    nearestN = i;
+                }
+            }
+        }
+        return nearestN;
+    }
+}
